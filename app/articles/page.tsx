ed8ae3be68859116a857638ca6b262b9abc57708
James Singleton <jamesrsingleton1@gmail.com>
2023-01-13 21:24:40 -0700

    feat: initial build

---
+++ app/articles/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Articles</h1>
+    </main>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  modularizeImports: {
+    '@heroicons/react/24/outline': {
+      transform: '@heroicons/react/24/outline/{{member}}',
+    },
+    '@heroicons/react/20/solid': {
+      transform: '@heroicons/react/20/solid/{{member}}',
+    },
+  },
+  images: {
+    domains: ['images.unsplash.com', 'tailwindui.com'],
+  },
 }
 
 module.exports = nextConfig
