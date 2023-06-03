647470cd635aef3df011f176763ffa4291928333
James Singleton <jamesrsingleton1@gmail.com>
2023-06-02 17:10:32 -0700

    feat: add feedback card to sidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import { EnvelopeIcon } from '@heroicons/react/24/outline'
 
 import SearchBar from '@/components/SearchBar'
 import {
@@ -151,6 +152,18 @@ const RightSidebar = async () => {
               </div>
             </Link>
           </article>
+        </div>
+      </div>
+      <div className="rounded-md bg-white p-8 shadow-lg">
+        <h2 className="mb-8 text-lg font-bold lg:text-2xl">Please Share Your Feedback</h2>
+        <div className="text-center">
+          <a
+            href="mailto:ron@maricopaseniorliving.org"
+            className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <EnvelopeIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
+            Email Us!
+          </a>
         </div>
       </div>
     </>
