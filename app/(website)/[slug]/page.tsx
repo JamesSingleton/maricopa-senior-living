4ec62f2ceb825f56af775ca10490a3e18000aef1
James Singleton <jamesrsingleton1@gmail.com>
2023-06-19 22:48:34 -0700

    fix: quick contacts not showing unline

--- app/(website)/[slug]/page.tsx
+++ app/(website)/[slug]/page.tsx
@@ -16,11 +16,8 @@ export default async function Page({ params }: { params: { slug: string } }) {
       <h1 className="inline-block align-middle text-4xl font-semibold capitalize sm:text-5xl md:text-6xl">
         {pageData.title}
       </h1>
-      <section className="pt-4">
-        <CustomPortableText
-          value={pageData.body}
-          paragraphClasses="prose prose-lg prose-indigo text-sm font-medium text-zinc-500 sm:text-base md:text-lg lg:max-w-none pt-4"
-        />
+      <section className="prose prose-lg prose-indigo pt-4 text-sm font-medium text-zinc-500 sm:text-base md:text-lg lg:max-w-none">
+        <CustomPortableText value={pageData.body} />
       </section>
     </>
   )
