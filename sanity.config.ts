5893f54ef92b0b3ee0ece79a01a74790a0527c7f
James Singleton <jamesrsingleton1@gmail.com>
2023-06-28 18:17:35 -0700

    chore: update sanity.api file

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
+import { projectId, dataset } from './lib/sanity.api'
 
 export default defineConfig({
   name: 'default',
   title: 'Maricopa Senior Living',
-  projectId: 'bksfw1p8',
-  dataset: 'production',
+  projectId: projectId,
+  dataset: dataset,
   plugins: [deskTool(), visionTool()],
   basePath: '/admin',
   apiVersion: '2023-04-27',
