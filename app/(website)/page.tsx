c5140327c55ffd5681a5cd19aa8bb7f103de0766
James Singleton <jamesrsingleton1@gmail.com>
2023-06-30 16:36:55 -0700

    chore: update home page text

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -36,83 +36,135 @@ export async function generateMetadata(
 export default async function Home() {
   return (
     <div className="rounded-md bg-white px-8 py-8 shadow-lg lg:px-4 lg:py-4">
-      <Image alt="Maricopa Senior Living" src={ImageSrc} />
+      <figure>
+        <Image alt="Maricopa Senior Living" src={ImageSrc} />
+        <figcaption className="mt-4 text-center text-sm italic text-zinc-500">
+          The wild horses, roaming free on Gila River Indian Community (GRIC) tribal lands, are a
+          favorite of Maricopa residents, young and old. They remind us that the spirit of freedom
+          is ageless, and that we can each have the lifestyle we aspire to with a little help, good
+          information and a plan.
+        </figcaption>
+      </figure>
       <div className="prose prose-zinc mx-auto pt-8 lg:pt-4">
         <h1>Welcome to Maricopa Senior Living!</h1>
         <p>
-          The wild horses of Maricopa seem to be a favorite of our seniors. So, it seemed reasonable
-          to incorporate them into our logo. They represent a spirit of freedom and lifestyle that
-          we all may envy! A freedom that many of us may be aspiring to in the later part of our own
-          lives.
+          Maricopa Senior Living is first and foremost a directory of resources and information. The
+          intent is to provide answers to your questions about your lifestyle, whether it&apos;s the
+          one you currently have or the one you want to achieve. The website is designed to be
+          user-friendly, easy to navigate and read. And, it&apos;s all in <b>one location!</b> Help
+          is a click away!
         </p>
         <p>
-          <span className="text-2xl font-bold">Maricopa Senior Living</span> is primarily a
-          directory or content management style website. It hasn&apos;t been designed to be flashy.
-          Instead, we hope that you find it easy to navigate and read. Its intent is to provide
-          access to some of the best resources available in one location so that you can find
-          answers to questions that you might have about senior living. In this latter third of our
-          lives, we are exposed to many new opportunities or challenges that we may have little or
-          no experience with. There are a number of articles included that will help you explore and
-          analyze solutions to these new opportunities and challenges. The content will continue to
-          grow!
-        </p>
-        <p>
-          Also included are some links that will help you stay in touch with local news stories,
-          weather and what&apos;s happening in the City. Links to the City&apos;s web page will help
-          you stay up to date with scheduled meetings of the City Council, Planning &amp; Zoning
-          Commission and other City committees. You can check What&apos;s New in City development
-          projects and zoning public meeting notices. Information on important transportation
-          happenings and pending legislative activities will be easy to follow.
-        </p>
-        <p>
-          <b>Local links</b> will give you access to the Hospice of the Valley and Arizona AARP
-          sites. Additional links provide quick access to the Arizona Department of Economic
-          Security or the Community Action Human Resource Agency (CAHRA). The <b>National links</b>{' '}
-          connect to some of the best resource sites available to seniors in the country to help
-          keep you informed. For enjoyment, check out the included information on{' '}
-          <b>Arizona Travel</b> or care for your pets. We think that we have a good start!
+          As seniors, our needs, challenges, and experiences change. Challenges come and go. This
+          site contains articles and information for you to explore as possible solutions. The
+          content will continually grow, providing resources for whatever topic you seek.
         </p>
         <h2>Navigating the website</h2>
         <p>
-          Use the header bar (the bar at the top of the page) to quickly access popular sites and
-          other useful sites for research on topics that may be important to you. For example, you
-          can quickly get to <b>InMaricopa</b> Magazine, the City&apos;s{' '}
-          <b>What&apos;s New Maricopa</b>, AARP and the National Council on Aging (NCOA). One of the
-          best local links might be the Pinal-Gila Council for Senior Citizens (PGCSC) site. The
-          PGCSC site includes access to their excellent <b>Resource Guide</b>that you can browse
-          online or even download. It is a great place to begin your research on any topic related
-          to seniors. For access to important contact information when you are in a hurry click on
-          Quick Contact.
+          Use the <b>header bar</b> (at the top of the page) to quickly access useful sites for
+          research on topics that may be of importance to you. These links can help you to stay
+          informed and aware of events and local activities.
+        </p>
+        <h3>The City of Maricopa website links</h3>
+        <ul>
+          <li>Provide easy access to the City website.</li>
+          <li>
+            Access to information about <b>What&apos;s New</b> in City Development projects.
+          </li>
+          <li>
+            Dates, times, agendas, supporting documents and locations for City Council meetings,
+            citizen-based boards and committees and information on public hearings regarding zoning
+            changes.
+          </li>
+          <li>
+            Other links will help to keep you up to date on pending legislation, transportation
+            news, events and other current news affecting our city.
+          </li>
+        </ul>
+        <h3>InMaricopa links</h3>
+        <ul>
+          <li>Access to local news and stories impacting the City.</li>
+          <li>
+            Access to the <b>New Resident and Visitor Guide</b>.
+          </li>
+          <li>
+            Access to the <b>Senior Section</b> articles.
+          </li>
+          <li>Traffic information and cameras.</li>
+        </ul>
+        <h3>Weather links</h3>
+        <ul>
+          <li>Interesting information about current weather, forecasts and weather safety.</li>
+        </ul>
+        <h3>Local links</h3>
+        <ul>
+          <li>
+            Agencies and organizations offering programs and services for Maricopa, like the
+            Pinal-Gila Council for Senior Citizens, Hospice of the Valley, AARP, the Arizona
+            Department of Economic Security, The Community Action Human Resource Agency (CAHRA), and
+            more.
+          </li>
+        </ul>
+        <h3>Quick Contacts</h3>
+        <ul>
+          <li>
+            Handy lists of numbers for emergency contacts, local medical resources, abuse and
+            personal safety hotlines and houses of worship.
+          </li>
+        </ul>
+        <h3>On the right side of the page</h3>
+        <ul>
+          <li>
+            <b>Search box</b> - allows you to enter your own search information.
+          </li>
+          <li>
+            <b>Categories and Tags</b> that provide quick access to topics of popular interest. For
+            fun, check out the Arizona Travel category or the Pets tab.
+          </li>
+          <li>
+            <b>Special Selection Blocks</b>
+            <ul>
+              <li>
+                <b>Joan&apos;s Corner</b> - Joan Koczor&apos;s familiar monthly newsletter (…keeping
+                you informed). Joan is a long-time local advocate for seniors and her popular
+                newsletter provides very useful and interesting information.
+              </li>
+              <li>
+                <b>City of Maricopa Community/Senior Center Monthly Calendar and Newsletter</b> -
+                all the publications regarding senior programming for the current month. The
+                calendar and newsletter can be viewed or downloaded.
+              </li>
+              <li>
+                <b>Ron&apos;s Ramblings</b> - a current article or information being highlighted.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <b>ADA (Americans with Disabilities Act) Accessibility Tool</b> along the right-hand
+            edge of the screen - look for an icon of a small person with outstretched arms. This is
+            the icon for an accessibility tool that can allow you to modify the look of the screen
+            in front of you for improved accessibility. Just click on the icon to see the available
+            menu options.
+          </li>
+          <li>
+            <b>The UP arrow</b> - in the lower right corner takes you to the top of the page.
+          </li>
+        </ul>
+        <p>
+          This website should be compatible with multiple platforms including desktops, tablets and
+          smart phones.
         </p>
         <p>
-          You can also use the search box to look for a particular term or character string. Use one
-          of the popular <b>Category</b> blocks or <b>Tags</b> in the side bar to quickly access the
-          information highlighted by those tools.
-        </p>
-        <p>
-          Finally, there are several specialized selection blocks that can take you to the{' '}
-          <b>Community/Senior Center</b> Calendar and newsletters for the month, Joan Koczor&apos;s
-          excellent monthly newsletter (<b>...keeping you informed</b>) and{' '}
-          <b>Ron&apos;s Ramblings</b> to highlight important new information as it occurs.
-        </p>
-        <p>
-          Use the go back to the top arrow in the lower right corner to get you back to the top
-          quickly. The more you play with it, the easier it will become. We&apos;ve incorporated
-          some ADA (Americans with Disabilities Act) tools to improve accessibility. The website
-          should be compatible with multiple platforms including tablets and smart phones. Feel free
-          to drop me a{' '}
+          We encourage feedback and suggestions! Feel free to{' '}
           <a
-            className="text-indigo-500 hover:text-indigo-600"
+            className="text-indigo-600 hover:text-indigo-700"
             href="mailto:ron@maricopaseniorliving.org"
           >
             email
           </a>{' '}
-          with suggestions, corrections and new ideas. Your assistance is greatly appreciated in
-          trying to get this site launched and, more importantly, useful to the intended users.
-        </p>
-        <p>
-          Enjoy your exploration! There&apos;s a lot of great information currently available for
-          your use. And it is going to continue to grow over time!
+          any ideas, comments, suggestions or challenges you discover. Feel free to let us know if
+          there&apos;s something you&apos;d like to see included. Your feedback is appreciated. We
+          thank you for taking the time to check our site out!
         </p>
       </div>
     </div>
