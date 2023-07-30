24b71ad8a86a2a286dbe98c1e5a3a8ac827210e3
James Singleton <jamesrsingleton1@gmail.com>
2023-07-30 15:59:51 -0700

    chore: use next-sanity/webhook

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,36 +1,36 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
-import { headers } from 'next/headers'
-import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook'
+import { parseBody } from 'next-sanity/webhook'
+
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 const secret = `${process.env.SANITY_WEBHOOK_SECRET}`
 
-export async function POST(request: NextRequest) {
-  console.log(`===== Webhook request ${request}`)
-  const res = await request.json()
+export async function POST(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const { isValidSignature, body } = await parseBody(request, secret)
 
-  const headersList = headers()
+    if (!isValidSignature) {
+      NextResponse.json({
+        success: false,
+        message: 'Invalid signature',
+      }, {
+        status: 401,
+      })
 
-  const signature = `${headersList.get(SIGNATURE_HEADER_NAME)}`
-  const isValid = isValidSignature(JSON.stringify(res), signature, secret)
+      return
+    }
 
-  console.log(`===== Is the webhook request valid? ${isValid}`)
+    console.log(`===== Received webhook =====`)
+    console.log(JSON.stringify(body, null, 2))
 
-  if (!isValid) {
-    NextResponse.json({ success: false, message: 'Invalid signature' }, { status: 401 })
-    return
+    if (body._type === 'service') {
+      revalidatePath('/')
+    }
+
+    return NextResponse.json({ revalidated: true, now: Date.now() })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.error()
   }
-
-  const { type, slug } = res
-
-  if (type === 'post') {
-    console.log(`===== Revalidating /articles/${slug.current}`)
-    revalidatePath(`/articles/${slug.current}`)
-  }
-  
-  if (type === 'service') {
-    console.log(`===== Service Title: ${res.title}`)
-  }
-
-  return NextResponse.json({ revalidated: true, now: Date.now() })
 }
