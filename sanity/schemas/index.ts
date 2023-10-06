fc6a631b97bb585f96ddcbe8c2db10f282fca032
James Singleton <jamesrsingleton1@gmail.com>
2023-10-05 21:19:47 -0700

    chore: remove unused document

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -4,7 +4,6 @@ import author from './documents/author'
 import tag from './documents/tag'
 import service from './documents/service'
 import blockContent from './blockContent'
-import menu from './documents/menu'
 import link from './objects/link'
 import page from './documents/page'
 import home from './singletons/home'
@@ -19,7 +18,6 @@ export const schemaTypes = [
   blockContent,
   tag,
   service,
-  menu,
   link,
   page,
   timeValueType,
