e53f189f9b37ed88b6a799f6c889f99b110e5e15
James Singleton <jamesrsingleton1@gmail.com>
2024-01-28 14:49:31 -0700

    chore: more fixes due to dep upgrade

--- app/(website)/articles/[slug]/page.tsx
+++ app/(website)/articles/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { notFound } from 'next/navigation'
 import { CalendarIcon } from '@heroicons/react/24/outline'
 
 import { getPostBySlug, getAllPostSlugs } from '@/lib/sanity.fetch'
-import { urlForImage } from '@/lib/sanity.image'
 import { CustomPortableText } from '@/components/CustomPortableText'
 import Date from '@/components/Date'
 import BackButton from '@/components/BackButton'
@@ -12,7 +11,7 @@ import ImageComponent from '@/components/ImageComponent'
 import type { Metadata } from 'next'
 
 export async function generateStaticParams() {
-  const slugs = (await getAllPostSlugs()) as string[]
+  const slugs = await getAllPostSlugs()
 
   return slugs.map((slug) => ({
     slug,
