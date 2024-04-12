7f01cedf4cf6b1041e6719e009901c920051f630
James Singleton <jamesrsingleton1@gmail.com>
2024-04-12 08:55:29 -0700

    feat: update deps

--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -2,7 +2,8 @@ import Studio from './Studio'
 
 export const dynamic = 'force-static'
 
-export { metadata } from 'next-sanity/studio/metadata'
+export { metadata } from 'next-sanity/studio'
+export { viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
   return <Studio />
